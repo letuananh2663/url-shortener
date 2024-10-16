c5b54a87738d41ea437c9b1d1d8d08ff3efcdac4
tuananh <letuananh2663@gmail.com>
2024-10-17 00:51:32 +0700

    GET route

--- components/url-list.tsx
+++ components/url-list.tsx
@@ -1,28 +1,56 @@
+"use client"
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from './ui/button'
 import { CopyIcon, EyeIcon } from 'lucide-react'
 
+type Url = {
+    id: string;
+    shortCode: string;
+    originUrl: string;
+    visits: number
+}
 export default function UrlList() {
+    const [urls, setUrls] = useState<Url[]>([]);
+
+    const shortenerUrl = (code: string) => `${process.env.NEXT_PUBLIC_BASE_URL}/${code}`;
+
+    const fetchUrls = async () => {
+        try {
+            const response = await fetch('/api/urls');
+            const data = await response.json();
+            setUrls(data);
+        } catch (error) {
+            console.error('Error fetching URLs', error)
+        }
+    }
+
+    useEffect(() => {
+        fetchUrls();
+    }, []);
+
     return (
         <div>
             <h2 className='text-2xl font-bold mb-2'>Recent URLs</h2>
             <ul className='space-y-2'>
-                <li className='flex justify-between items-center gap-2'>
-                    <Link href='https://github.com/letuananh2663' target='_blank' className='text-blue-500'>
-                        https://github.com/letuananh2663
-                    </Link>
-                    <div className='flex items-center gap-2'>
-                        <Button variant="ghost" size='icon' className='text-muted-foreground hover:bg-muted'>
-                            <CopyIcon className='w-4 h-4' />
-                            <span className='sr-only'>Copy to Clipboard</span>
-                        </Button>
-                        <span className='flex items-center'>
-                            <EyeIcon className='w-4 h-4' />
-                            1 views
-                        </span>
-                    </div>
-                </li>
+                {urls.map((url) => (
+                    <li key={url.id} className='flex justify-between items-center gap-2'>
+                        <Link href={`/${url.shortCode}`} target='_blank' className='text-blue-500'>
+                            {shortenerUrl(url.shortCode)}
+                        </Link>
+                        <div className='flex items-center gap-2'>
+                            <Button variant="ghost" size='icon' className='text-muted-foreground hover:bg-muted'>
+                                <CopyIcon className='w-4 h-4' />
+                                <span className='sr-only'>Copy to Clipboard</span>
+                            </Button>
+                            <span className='flex items-center gap-2'>
+                                <EyeIcon className='w-4 h-4' />
+                                {url.visits}
+                            </span>
+                        </div>
+                    </li>
+                ))}
             </ul>
         </div>
     )
