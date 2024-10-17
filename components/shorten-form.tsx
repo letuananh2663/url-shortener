0e4030d6d1e872402b849174830b539d0dab5fb5
tuananh <letuananh2663@gmail.com>
2024-10-17 12:58:45 +0700

    Refresh URL List

--- components/shorten-form.tsx
+++ components/shorten-form.tsx
@@ -4,7 +4,11 @@ import React, { useState } from 'react'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
 
-export default function ShortenForm() {
+interface ShortenFormProps {
+    handleUrlShortened: () => void;
+}
+
+export default function ShortenForm({ handleUrlShortened }: ShortenFormProps) {
     const [url, setUrl] = useState<string>('');
 
     const handleSubmit = async (e: React.FormEvent) => {
@@ -20,6 +24,7 @@ export default function ShortenForm() {
             });
             await response.json();
             setUrl('');
+            handleUrlShortened();
         } catch (error) {
             console.error('Error shortening URL: ', error);
         } finally {
--- components/url-shortener-container.tsx
+++ components/url-shortener-container.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
 import ShortenForm from "./shorten-form";
 import UrlList from "./url-list";
 
 export default function UrlShortenerContainer() {
-    return <div>
-        <ShortenForm />
-        <UrlList />
-    </div>
+    const [refreshKey, setRefreshKey] = useState(0);
+
+    const handleUrlShortened = () => {
+        setRefreshKey((prev) => prev + 1);
+    };
+
+    return (
+        <div>
+            <ShortenForm handleUrlShortened={handleUrlShortened} />
+            <UrlList key={refreshKey} />
+        </div>
+    )
 }
